3568dd35d9af8929cc99c50205fc6faf50b0a707
Umut Yiğit <93212416+NorthHopee@users.noreply.github.com>
2025-11-05 02:07:40 +0300

    gemedelete

--- newproject/Assets/LatterData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "LetterData", menuName = "Arabic Letter")]
-public class LatterData : ScriptableObject
-{
-    public Sprite letterSprite;   // Harfin görseli
-    public AudioClip soundClip;   // Okunuþ sesi
-}
